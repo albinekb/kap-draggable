1cffe2b9266d59ab04c96662641059c6f822ceb3
Albin Ekblom <albinekblom@gmail.com>
2018-03-24 16:35:57 +0100

    🎉 Make window native

--- index.js
+++ index.js
@@ -2,49 +2,51 @@ const electron = require('electron')
 const { BrowserWindow, ipcMain: ipc } = electron
 const { join: pathJoin } = require('path')
 
-const draggableFile = async (gifPath) => {
+const draggableFile = async (gifPath, { title }) => {
   const { bounds } = electron.screen.getPrimaryDisplay()
   const height = 200
   const width = 300
   let win = new BrowserWindow({
     width,
     height,
-    x: ((bounds.width / 2) + (bounds.width / 4)) - (width / 2),
-    y: (bounds.width.height / 2) - (height / 2),
+    x: bounds.width / 2 + bounds.width / 4 - width / 2,
+    y: bounds.width.height / 2 - height / 2,
     alwaysOnTop: true,
     acceptFirstMouse: true,
-    frame: false,
+    // frame: false,
     enableLargerThanScreen: true,
     movable: true,
-    resizable: false
+    title,
+    resizable: true,
+    backgroundColor: 'black',
   })
 
   win.setAlwaysOnTop(true, 'screen-saver')
-  win.loadURL(`file://${__dirname}/index.html`)
+  win.loadURL(`file://${__dirname}/index.html#${gifPath}`)
 
   win.on('closed', () => {
     win = null
   })
 
-  ipc.once('ondragstart', (event) => {
+  ipc.once('ondragstart', event => {
     event.sender.startDrag({
       file: gifPath,
-      icon: pathJoin(__dirname, 'gif.png')
+      icon: pathJoin(__dirname, 'gif.png'),
     })
     win.close()
   })
 }
 
 const action = async context => {
-  if (context.format !== 'gif') throw new Error('Only supports gif')
   const file = await context.filePath()
-  draggableFile(file)
+  const title = context.defaultFileName
+  draggableFile(file, { title })
 }
 
 const draggable = {
   title: 'Drag and Drop',
-  formats: ['gif'],
-  action
+  formats: ['gif', 'mp4'],
+  action,
 }
-
+exports.action = action
 exports.shareServices = [draggable]
